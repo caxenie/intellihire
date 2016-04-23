157db1cdc25911b18d7a3245bdf4322f85b67ef2
Klaus Ondrag <klaus.ondrag@outlook.com>
2016-04-23 23:23:55 +0200

    add fake implementation, returns data based on input

--- backend/src/main/java/com/intellihire/service/ComputeService.java
+++ backend/src/main/java/com/intellihire/service/ComputeService.java
@@ -3,22 +3,22 @@ package com.intellihire.service;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Klaus on 23.04.2016.
  */
 @Path("/compute")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
 public class ComputeService {
-    private final String DEFAULT_FORMAT = "{'Sales': %.2f, 'Tester': %.2f"
-                                            + ", Algorithms': %.2f, 'Developer': %.2f"
-                                            + ", 'Architect': %.2f}";
+    private final String DEFAULT_FORMAT = "{\"Sales\": %.2f, \"Tester\": %.2f"
+                                            + ", \"Algorithms\": %.2f, \"Developer\": %.2f"
+                                            + ", \"Architect\": %.2f}";
 
     @GET
     @Path("/mock")
+    @Produces({ MediaType.APPLICATION_JSON })
     public Response mock() {
 
         double[] values = {0.19, 0.43, 0.52, 0.87, 0.55};
@@ -29,12 +29,26 @@ public class ComputeService {
     }
 
     @POST
-    @Path("/")
-    public Response test(List<Integer> input) {
+    @Path("/fake")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response test(String json) {
+        Pattern pattern = Pattern.compile("\\s");
+        Matcher matcher = pattern.matcher(json);
+        json = matcher.replaceAll("");
 
-        String output = String.format(Locale.ENGLISH, DEFAULT_FORMAT, input.get(0), input.get(1), input.get(2),
-                input.get(3), input.get(4));
+        pattern = Pattern.compile("\\{\"input\":\\[([\\d, ]*)\\]\\}");
+        matcher = pattern.matcher(json);
+        matcher.matches();
+        json = matcher.replaceAll(matcher.group(1));
 
+        String[] input = json.split(",");
+        double[] values = new double[input.length];
+        for (int i = 0; i < input.length; i++) {
+            values[i] = 1 / Double.parseDouble(input[i]);
+        }
+        String output = String.format(Locale.ENGLISH, DEFAULT_FORMAT, values[0], values[1], values[2],
+                values[3], values[4]);
         return Response.status(200).entity(output).build();
     }
 }
