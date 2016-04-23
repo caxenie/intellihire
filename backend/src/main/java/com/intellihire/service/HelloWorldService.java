09eed8aaf23ea7e80f78f761b2543d785a735da4
Klaus Ondrag <klaus.ondrag@outlook.com>
2016-04-23 15:10:08 +0200

    add hello world rest service

---
+++ backend/src/main/java/com/intellihire/service/HelloWorldService.java
@@ -0,0 +1,35 @@
+package com.intellihire.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by Klaus on 23.04.2016.
+ */
+@Path("/hello")
+public class HelloWorldService {
+
+    @GET
+    @Path("/{name}")
+    public Response helloName(@PathParam("name") String name) {
+
+        String output = "Hello " + name;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+    @GET
+    @Path("/")
+    public Response helloWorld() {
+
+        String output = "Hello World!";
+
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+}
